581fb3630a9765fd5e2ef2fda82e78c7165947fa
Vladimir L <eisomt@Vladimirs-MacBook-Air.local>
2022-07-11 10:08:26 +0300

    Adds fixes

--- js/markup.js
+++ js/markup.js
@@ -4,19 +4,36 @@ const cardTemplate = document.querySelector('#card').content;
 const list = document.querySelector('.map__canvas');
 const card = cardTemplate.querySelector('.popup');
 
+console.log(generatedAds);
+
 for (let i = 0; i < generatedAds.length; i++) {
+
   const newCard = card.cloneNode(true);
+
   newCard.querySelector('.popup__title').textContent = generatedAds[i].offer.title;
   newCard.querySelector('.popup__text--address').textContent = generatedAds[i].offer.address;
   newCard.querySelector('.popup__text--price').textContent = `${generatedAds[i].offer.price} ₽/ночь`;
+
   const cardType = newCard.querySelector('.popup__type');
   const generatedType = generatedAds[i].offer.type;
-  cardType.textContent = generatedType === 'flat' ? 'Квартира'
-  : generatedType === 'bungalow' ? 'Бунгало'
-  : generatedType === 'house' ? 'Дом'
-  : generatedType === 'palace' ? 'Дворец'
-  : generatedType === 'hotel' ? 'Отель'
-  : null;
+
+  const housingTypes = {
+    flat: 'Квартира',
+    bungalow: 'Бунгало',
+    house: 'Дом',
+    palace: 'Дворец',
+    hotel: 'Отель'
+  };
+
+  const typesKeys = Object.keys(housingTypes);
+  const typesValues = Object.values(housingTypes);
+
+  for (let j = 0; j < typesKeys.length && j < typesValues.length; j++) {
+    if (typesKeys[j] === generatedType) {
+      cardType.textContent = typesValues[j];
+    }
+  }
+
   newCard.querySelector('.popup__text--capacity').textContent = `${generatedAds[i].offer.rooms} комнаты для ${generatedAds[i].offer.guests} гостей`;
   newCard.querySelector('.popup__text--time').textContent =  `Заезд после ${generatedAds[i].offer.checkin}, выезд до ${generatedAds[i].offer.checkout}`;
 
@@ -36,28 +53,14 @@ for (let i = 0; i < generatedAds.length; i++) {
   const photosList = newCard.querySelector('.popup__photos');
   const photo = newCard.querySelector('.popup__photo');
   const generatedPhotos = generatedAds[i].offer.photos;
-
   photo.src = generatedPhotos[0];
   generatedPhotos.shift();
-
-  console.log(generatedPhotos);
 
   generatedPhotos.forEach((item) => {
     const newPhoto = photo.cloneNode(true);
     photosList.appendChild(newPhoto);
     newPhoto.src = item;
   });
-
-  //   for (let j = 0; j < generatedPhotos.length; j++) {
-  //   const newPhoto = photo.cloneNode(true);
-  //   photosList.appendChild(newPhoto);
-  //   newPhoto.src = generatedPhotos[j];
-  // }
-
-    // for (const item of generatedPhotos) {
-  //   photosList.append(newPhoto);
-  //   newPhoto.src = item;
-  // }
 
   const avatar = newCard.querySelector('.popup__avatar');
   avatar.src = generatedAds[i].author.avatarURL;
